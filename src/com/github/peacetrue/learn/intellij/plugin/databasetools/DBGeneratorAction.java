460461e325dfaaffc0fad04e842bc187a482a723
xiayx <xiayouxue@hotmail.com>
2019-05-22 14:38:53 +0800

    在DatabaseTools上添加一个菜单项

---
+++ src/com/github/peacetrue/learn/intellij/plugin/databasetools/DBGeneratorAction.java
@@ -0,0 +1,17 @@
+package com.github.peacetrue.learn.intellij.plugin.databasetools;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author xiayx
+ */
+public class DBGeneratorAction extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        
+    }
+
+}
